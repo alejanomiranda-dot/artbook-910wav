5efd8c29cbcced62e422a73c9c97cbbd1d15f190
alejanomiranda-dot <alejano.miranda@gmail.com>
2025-12-01 00:55:37 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "TU_URL_DE_SUPABASE";
-const supabaseKey = "TU_PUBLIC_KEY";
+const supabaseUrl = "https://supabase.com/dashboard/project/qjeleezjbecjpoirrhqe/settings/api-keys";
+const supabaseKey = "sb_publishable_UrmYOoxd08ksuhSU3b-mKQ_SaPzTZCC";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
